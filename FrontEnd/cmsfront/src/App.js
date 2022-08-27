c47e8833ca8b6987f1e9300e590dd93ed6f2d7e8
Managedcode-ux <71963227+Managedcode-ux@users.noreply.github.com>
2022-08-27 13:43:24 +0530

    FrontEnd overhaul

---
+++ FrontEnd/cmsfront/src/App.js
@@ -0,0 +1,11 @@
+import 'tw-elements';
+import CustomerList from './Pages/CustomerList';
+function App() {
+  return (
+    <div className="App ">
+      <CustomerList />
+    </div>
+  );
+}
+
+export default App;
---
+++ FrontEnd/cmsfront/src/Pages/CustomerList.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../CommonComponets/NavBar";
+
+const CustomerList = () => {
+  return (
+    <div>
+      <NavBar />
+
+    </div>
+  );
+}
+
+export default CustomerList;
